d567a0fd10d38b45ebdf17b27fe53072c9561bc1
dmitry-sharashkin <faustfbs@mail.ru>
2021-11-26 22:28:32 +0300

    Вёрстка Header и Sidebar

---
+++ src/components/Header/header.tsx
@@ -0,0 +1,52 @@
+import React from "react";
+import s from './Header.module.css'
+import {faSearch, faChevronDown} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+import photo from '../../assets/img.png'
+
+
+const Header = () => {
+    return (
+        <header>
+            <div className={s.container}>
+                <div className={s.left}>
+                    <time  className={s.time}>Cреда, 13 окт</time>
+                    <div className={s.analytics}>
+                        <div className={s.analyticsItem}>
+                            <div className={s.text}>Новые звонки <span className={s.green}>20 из 30 шт</span></div>
+                            <div className={s.progressBar}>
+                                <div className={s.progress + ' ' + s.green}></div>
+                            </div>
+                        </div>
+                        <div className={s.analyticsItem}>
+                            <div className={s.text}>Качество разговоров <span className={s.yellow}>40%</span></div>
+                            <div className={s.progressBar}>
+                                <div className={s.progress + ' ' + s.yellow}></div>
+                            </div>
+                        </div>
+                        <div className={s.analyticsItem}>
+                            <div className={s.text}>Конверсия в заказ <span className={s.red}>67%</span></div>
+                            <div className={s.progressBar}>
+                                <div className={s.progress + ' ' + s. red}></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div className={s.right}>
+                    <FontAwesomeIcon className={s.search} icon={faSearch}/>
+                    <div className={s.userData}>
+                        <div className={s.name}>
+                            ИП Сидорова Александра Михайловна
+                            <FontAwesomeIcon icon={faChevronDown}/>
+                        </div>
+                        <div className={s.photo}>
+                            <img src={photo}/>
+                            <FontAwesomeIcon icon={faChevronDown}/>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </header>
+    )
+}
+export default Header
---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import logo from '../../assets/logo.png';
+import s from './Sidebar.module.css';
+import {
+    faPhone,
+    faEnvelope,
+    faCheckDouble,
+    faChartLine,
+    faUserFriends,
+    faBookReader,
+    faBriefcase,
+    faUser,
+    faFileAlt,
+    faCog,
+    faPlusCircle,
+    faExclamationCircle
+} from '@fortawesome/free-solid-svg-icons';
+import {FontAwesomeIcon} from "@fortawesome/react-fontawesome";
+
+const Sidebar = () => {
+    return (
+        <aside className={s.sidebar}>
+            <nav>
+                <ul>
+
+                    <li className={s.logoBox}><img src={logo}/></li>
+                    <li className={s.item}><FontAwesomeIcon className={s.icon} icon={faChartLine}/>Итоги</li>
+                    <li className={s.item}><FontAwesomeIcon className={s.icon} icon={faCheckDouble}/>Заказы</li>
+                    <li className={s.item}><FontAwesomeIcon className={s.icon} icon={faEnvelope}/>Сообщения</li>
+                    <li className={s.item + ' ' + s.activeItem}><FontAwesomeIcon className={s.icon} icon={faPhone}/>Звонки
+                    </li>
+                    <li className={s.item}><FontAwesomeIcon className={s.icon} icon={faUserFriends}/>Контрагенты</li>
+                    <li className={s.item}><FontAwesomeIcon className={s.icon} icon={faFileAlt}/>Документы</li>
+                    <li className={s.item}><FontAwesomeIcon className={s.icon} icon={faUser}/>Исполнители</li>
+                    <li className={s.item}><FontAwesomeIcon className={s.icon} icon={faBriefcase}/>Отчеты</li>
+                    <li className={s.item}><FontAwesomeIcon className={s.icon} icon={faBookReader}/>База знаний</li>
+                    <li className={s.item}><FontAwesomeIcon className={s.icon} icon={faCog}/>Настройки</li>
+                </ul>
+                <div className={s.buttonsContainer}>
+                    <button className={s.addOrder}>Добавить заказ <FontAwesomeIcon className={s.iconBtn} icon={faPlusCircle}/></button>
+                    <button className={s.pay}>Отплата <FontAwesomeIcon className={s.iconBtn} icon={faExclamationCircle}/></button>
+                </div>
+            </nav>
+        </aside>
+    )
+}
+export default Sidebar
